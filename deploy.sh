f7025893ce9fdbdc5346a61598e0aed4aca2ca5e
Joshua Bright <tryer3000@163.com>
2018-04-16 08:58:18 +0800

    deploy script

---
+++ deploy.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+if [[ $(git status -s) ]]
+then
+    echo "The working directory is dirty. Please commit any pending changes."
+    exit 1;
+fi
+
+echo "Deleting old publication"
+rm -rf public
+git worktree prune
+mkdir public
+rm -rf .git/worktrees/public/
+
+echo "Checking out gh-pages branch into public"
+git branch -D gh-pages
+git worktree add -b gh-pages public origin/gh-pages
+
+echo "Removing existing files"
+rm -rf public/*
+
+echo "Generating site"
+hugo
+echo "Updating gh-pages branch"
+cd public
+echo blog.tryer3000.cc >> CNAME
+git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
+cd ..
